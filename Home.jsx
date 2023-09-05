ed611a681734e7144ffe37d3d1f0d572a0f018ed
Gopikasuresh24 <143775934+Gopikasuresh24@users.noreply.github.com>
2023-09-05 14:35:21 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,26 @@
+import Destination from "../components/Destination";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1610730668414-a9f774d34473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+        title="Happy Journey Happy Moments"
+        text="Select Your Favourite Spot."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
+      <Destination />
+      <Trip />
+      <Footer />
+    </>
+  );
+}
+export default Home;
