bfd2a3976782e98096ce1a7ab6611cbeb5109c1e
Gopikasuresh24 <143775934+Gopikasuresh24@users.noreply.github.com>
2023-09-05 14:28:05 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,24 @@
+import "./FooterStyles.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div>
+          <h1>The Traveler</h1>
+          <p>Select Your Favourite Spot!</p>
+        </div>
+        <div>
+          <a href="/">
+            <i className="fa-brands fa-facebook-square"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-instagram-square"></i>
+          </a>
+          
+        </div>
+      </div>    
+      </div>
+  );
+};
+export default Footer;
